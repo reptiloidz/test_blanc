29792e5f5d3988de65b9413d933ff0fea7ad8933
voronetslrk <v.voroncova@blanc.ru>
2024-07-01 01:10:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app.routes';
+import BaseComponent from 'bootstrap/js/dist/base-component';
+import { BasketComponent } from './basket/basket.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AppRoutingModule, RouterOutlet, BasketComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AccountOwnerClient';
+
+  @HostBinding('class') class='d-flex flex-column h-100'
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BasketComponent } from './basket/basket.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'basket', component: BasketComponent }
+];
+
+@NgModule({
+  imports: [],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
